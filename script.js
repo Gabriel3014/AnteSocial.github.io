d96b0a4b32473a2bbe9fd33138080f5d888d7b89
AnteSocial <39884930+AnteSocial@users.noreply.github.com>
2018-06-06 16:36:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,29 +2,29 @@ function a(id){return document.getElementById(id)};
 var game = {
 	replicanti: {
 		display: a("replicanti"),
-		amount: new Decimal(1),
-		chance: new Decimal(10)
+		amount: 1,
+		chance: 10
 	},
 	updateHTML: function(){
-		game.replicanti.display.innerHTML = " " + game.replicanti.amount.floor();
+		game.replicanti.display.innerHTML = game.replicanti.amount;
 	},
 	replicantiUpdate: function(){
 		window.setInterval(function(){
-			if(game.replicanti.amount.lte(100)){
-				for(i=0;game.replicanti.amount.lte(i);i++){
-					if(game.replicanti.chance.lt(Math.random()*100)){
-						game.replicanti.amount.plus(1);
+			if(game.replicanti.amount < 100){
+				for(i=0;i<game.replicanti.amount;i++){
+					if(Math.random()*100<game.replicanti.chance)){
+						game.replicanti.amount+=1;
 						game.updateHTML()
 					}
 				}
-			} else if(game.replicanti.amount.gte(100)) {
+			} else if(game.replicanti.amount>100) {
 				let x = 0
 				for(i=0;i<100;i++){
-					if(game.replicanti.chance.lt(Math.random()*100)){
+					if(Math.random()*100<game.replicanti.chance)){
 						x++
 					}
 				}
-				game.replicanti.amount = game.replicanti.amount.mul(1+x/100).floor()
+				game.replicanti.amount = Math.floor(game.replicanti.amount*(1+x/100))
 				game.updateHTML()
 			}
 		},1000)
