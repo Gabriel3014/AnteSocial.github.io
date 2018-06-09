d237b55a2c57e64697c368b2a96840244fe59ab3
AnteSocial <39884930+AnteSocial@users.noreply.github.com>
2018-06-09 12:39:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function a(id){return document.getElementById(id)};
 var game = {
 	replicanti: {
 		display: a("replicanti"),
-		amount: 1,
-		chance: 10,
-		tickspeed: 1000,
+		amount: new Decimal(1),
+		chance: new Decimal(10),
+		tickspeed: new Decimal(1000),
 	},
 	upgrades: {
-		chance: 1000,
-		tickspeed: 10,
+		chance: new Decimal(1000),
+		tickspeed: new Decimal(10),
 		chanceDisplay: a("replicate_chance"),
 		chancePriceDisplay: a("replicate_price"),
 		tickspeedDisplay: a("tickspeed"),
@@ -23,21 +23,21 @@ var game = {
 	},
 	replicantiUpdate: function(){
 		game.updateHTML()
-		if(game.replicanti.amount <= 100){
-			for(i=0;i<game.replicanti.amount;i++){
+		if(game.replicanti.amount.lt(100)){
+			for(i=0;game.replicanti.amount.lt(i);i++){
 				if(Math.random()*100<game.replicanti.chance){
 					game.replicanti.amount+=1;
 					game.updateHTML()
 				}
 			}
-		} else if(game.replicanti.amount>100) {
+		} else if(game.replicanti.amount.gt(100) {
 			let x = 0
 			for(i=0;i<100;i++){
-				if(Math.random()*100<game.replicanti.chance){
+				if(game.replicanti.chance.gt(Math.random()*100){
 					x++
 				}
 			}
-			game.replicanti.amount = Math.floor(game.replicanti.amount*(1+x/100))
+			game.replicanti.amount = game.replicanti.amount.mul(1+x/100).floor()
 			game.updateHTML()
 		}
 	}
@@ -46,22 +46,22 @@ var game = {
 var gameLoop = window.setInterval(game.replicantiUpdate, game.replicanti.tickspeed)
 
 game.upgrades.chancePriceDisplay.onclick = function(){
-	if(game.replicanti.amount > game.upgrades.chance){
+	if(game.replicanti.amount.gt(game.upgrades.chance)){
 		if(game.replicanti.chance < 100){
-			game.replicanti.amount -= game.upgrades.chance;
-			game.upgrades.chance *= 1e4;
-			game.replicanti.chance += 1;
+			game.replicanti.amount.sub(game.upgrades.chance);
+			game.upgrades.chance.mul(10);
+			game.replicanti.chance.add(1);
 			game.updateHTML();
 		}
 	}
 }
 
 game.upgrades.tickspeedPriceDisplay.onclick = function(){
-	if(game.replicanti.amount > game.upgrades.tickspeed){
-		game.replicanti.amount -= game.upgrades.tickspeed
-		game.upgrades.tickspeed *= 10;
+	if(game.replicanti.amount.gt(game.upgrades.tickspeed)){
+		game.replicanti.amount.sub(game.upgrades.tickspeed);
+		game.upgrades.tickspeed.mul(10);
 		clearInterval(gameLoop)
-		game.replicanti.tickspeed = (game.replicanti.tickspeed/100)*90;
+		game.replicanti.tickspeed = (game.replicanti.tickspeed.div(100)).mul(90);
 		game.updateHTML();
 		gameLoop = window.setInterval(game.replicantiUpdate, game.replicanti.tickspeed)
 	}
